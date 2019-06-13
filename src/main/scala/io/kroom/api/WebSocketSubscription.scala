53b444633f812a975071a417381ec585f7021884
cepalle <4YNj:3i,-=>
2019-06-13 10:30:07 +0200

    add OperationMessage

--- src/main/scala/io/kroom/api/WebSocketSubscription.scala
+++ src/main/scala/io/kroom/api/WebSocketSubscription.scala
@@ -7,55 +7,31 @@ import akka.http.scaladsl.server.Directives
 import akka.stream.{ActorMaterializer, FlowShape, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Sink, Source}
 import slick.jdbc.H2Profile
-import io.circe.generic.auto._
-import io.circe.{Json, parser}
-import io.circe.syntax._
-
 import scala.util.{Success, Try}
-
 
 // ---
 
-/*
-export default class MessageTypes {
-  public static GQL_CONNECTION_INIT = 'connection_init'; // Client -> Server
-  public static GQL_CONNECTION_ACK = 'connection_ack'; // Server -> Client
-  public static GQL_CONNECTION_ERROR = 'connection_error'; // Server -> Client
-
-  // NOTE: The keep alive message type does not follow the standard due to connection optimizations
-  public static GQL_CONNECTION_KEEP_ALIVE = 'ka'; // Server -> Client
-
-  public static GQL_CONNECTION_TERMINATE = 'connection_terminate'; // Client -> Server
-  public static GQL_START = 'start'; // Client -> Server
-  public static GQL_DATA = 'data'; // Server -> Client
-  public static GQL_ERROR = 'error'; // Server -> Client
-  public static GQL_COMPLETE = 'complete'; // Server -> Client
-  public static GQL_STOP = 'stop'; // Client -> Server
-}*/
-
 object ApolloProtocol {
 
-  /*
-  export interface OperationMessage {
-    payload?: any;
-    id?: string;
-    type: string;
-  }
-  */
+  case class OperationMessage(
+                               payload: Option[String], // payload?: any;
+                               id: Option[String], // id?: string;
+                               `type`: String, // type: string;
+                             )
 
-  // Client -> Server
-  val GQL_CONNECTION_INIT = "GQL_CONNECTION_INIT" // payload: Object
-  val GQL_START = "GQL_START" // id + payload: {query, variables, operationName}
-  val GQL_STOP = "GQL_STOP" // id
-  val GQL_CONNECTION_TERMINATE = "GQL_CONNECTION_TERMINATE"
-  // Server -> Client
-  val GQL_CONNECTION_ACK = "GQL_CONNECTION_ACK"
-  val GQL_CONNECTION_ERROR = "GQL_CONNECTION_ERROR"
-  val GQL_DATA = "GQL_DATA" // id + payload: {data, errors}
-  val GQL_ERROR = "GQL_ERROR" // id + payload: Error
-  val GQL_COMPLETE = "GQL_COMPLETE" // id
-  val GQL_CONNECTION_KEEP_ALIVE = "GQL_CONNECTION_KEEP_ALIVE"
+  val GQL_CONNECTION_INIT = "connection_init" // Client -> Server
+  val GQL_CONNECTION_ACK = "connection_ack" // Server -> Client
+  val GQL_CONNECTION_ERROR = "connection_error" // Server -> Client
 
+  // NOTE: The keep alive message type does not follow the standard due to connection optimizations
+  val GQL_CONNECTION_KEEP_ALIVE = "ka" // Server -> Client
+
+  val GQL_CONNECTION_TERMINATE = "connection_terminate" // Client -> Server
+  val GQL_START = "start" // Client -> Server
+  val GQL_DATA = "data" // Server -> Client
+  val GQL_ERROR = "error" // Server -> Client
+  val GQL_COMPLETE = "complete" // Server -> Client
+  val GQL_STOP = "stop" // Client -> Server
 }
 
 // ---
@@ -74,7 +50,7 @@ case class SubscriptionAccepted() extends WSEvent
 
 case class Subscribe(query: String, operation: Option[String]) extends WSEvent
 
-case class QueryResult(json: Json) extends WSEvent
+case class QueryResult(json: String) extends WSEvent
 
 // ---
 
@@ -119,8 +95,8 @@ class WebSocketSubscription(val db: H2Profile.backend.Database)
     Flow.fromGraph(GraphDSL.create(subActorSource) { implicit builder =>
       subActorSourceCp =>
         import GraphDSL.Implicits._
+        import ApolloProtocol._
 
-        println("ici2")
         val materialization = builder.materializedValue.map(subActorRef => WSEventUserJoined(token, subActorRef))
         val merge = builder.add(Merge[WSEvent](2))
         val subActorSink = Sink.actorRef[WSEvent](subActorHandler, WSEventUserQuit())
@@ -136,11 +112,14 @@ class WebSocketSubscription(val db: H2Profile.backend.Database)
         })
 
         val WSEventsToMessages = builder.add(Flow[WSEvent].map {
+
           // serialization
           case a =>
-            println("ici66", a)
-            TextMessage("{\"type\":\"start\"}")
-
+            import io.circe.syntax._
+            import io.circe.generic.auto._
+            val res = OperationMessage(None, None, GQL_CONNECTION_ACK).asJson.toString()
+            println("ici66", a, res)
+            TextMessage(res)
         })
 
         materialization ~> merge ~> subActorSink
